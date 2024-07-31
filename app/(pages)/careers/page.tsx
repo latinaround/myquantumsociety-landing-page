2a81689defb3fdc66a09865c84c04cc1c093ca22
maxsivry <max.ivry@qaintum.ai>
2024-07-31 07:49:22 -0500

    changes to website

--- app/(pages)/careers/page.tsx
+++ app/(pages)/careers/page.tsx
@@ -15,7 +15,7 @@ const CareersPage: NextPage<Props> = ({ params }) => {
         <div className="absolute inset-0 bg-cover bg-center opacity-50" style={{ backgroundImage: "url('/images/design1.png')" }}></div>
         <h1 className="sm:text-6xl text-4xl text-white font-bold relative">Careers</h1>
       </div>
-      <div className="min-h-screen p-6">
+      <div className="min-h-screen p-6" style={{ backgroundColor: "var( oklch(var(--bc) / 1))" }}>
         <div className="container mx-auto text-white">
           <p className="mt-4">
             We are at the forefront of the Quantum Computing and Generative AI revolution. Our work with quantum large language models and Photonic QPUs as AI accelerators requires talented ML/AI research engineers and physicists.
